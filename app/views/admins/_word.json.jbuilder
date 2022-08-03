29a7aa794aea534c0c8d45d19c24ad2a172959ca
shivathmajshenoym <shivathmajshenoy@yahoo.com>
2022-08-03 16:18:05 +0530

    Tried to implement Api dictionary using new controller

---
+++ app/views/admins/_word.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! word, :id, :word_name, :word_def, :created_at, :updated_at
+json.url word_url(word, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :admins
+  resources :words
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/user' => 'admins#user'
   get '/error' => 'admins#error'
